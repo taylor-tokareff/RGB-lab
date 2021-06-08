5a126aed77d05624f7cba9bc1a50317691aefb44
taylor-tokareff <taylortokareff@gmail.com>
2021-06-07 17:36:23 -0700

    got stuff working

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,22 +1,28 @@
-const pool = require('../lib/utils/pool');
-const setup = require('../data/setup');
-const request = require('supertest');
-const app = require('../lib/app');
-const getName = require('../lib/app');
+
+// const request = require('supertest');
+// const app = require('../lib/app');
+import getName from '../lib/functions.js';
 
 describe('RGB-Lab routes', () => {
-  beforeEach(() => {
-    return setup(pool);
+  // beforeEach(() => {
+  //   return setup(pool);
+  // });
+
+  it('this is a test that will return the name in the object', () => {
+
+    const spot = { name: 'spot', age: 5, weight: '20 lbs' };
+    const actual = getName(spot); // returns spot
+    expect(actual).toEqual('spot');
+
 
   });
 
   it('this is a test that will return the name in the object', () => {
 
-    const spot = { name: 'spot', age: 5, weight: '20 lbs' };
-    getName(spot); // returns spot
-
     const character = { _id: '5cf5679a915ecad153ab68c9', name: 'Aang' };
-    getName(character); // returns Aang
+    const actual = getName(character); // returns Aang
+    expect(actual).toEqual('Aang');
+
 
 
   });
